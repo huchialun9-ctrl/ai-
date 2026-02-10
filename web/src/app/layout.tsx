eff83e1497c308fd50b5a7072bdd678ea6a25782
簡毓芳 <jianyufang@jianyufangs-MacBook-Air.local>
2026-02-10 15:15:30 +0800

    fix: import globals.css in layout.tsx

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "@/components/session-provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
